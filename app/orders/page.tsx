249ea7aae6d73374343e8165241353240d761d89
Ihsan <ali.ihsan.mv@gmail.com>
2023-10-11 20:19:55 +0500

    updated orders view

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -10,10 +10,7 @@ export default async function MenuPage({ searchParams }
     const paginatedList = await orderRepo.getByPage(Number(page));
 
     return <main className="flex min-h-screen flex-col items-center justify-top">
-            <h1>Orders</h1>
-            <p>{ paginatedList.items.length }</p>
-            <br />
-            {/* <MenuItemForm /> */}
+            <h1 className="font-bold pt-3">Orders</h1>
             <br />
             <section className="flex flex-row justify-center min-w-full flex-wrap gap-5">
                 {
