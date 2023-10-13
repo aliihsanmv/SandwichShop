27faa7366a36c05a719696979b7c4476d746a39b
Ihsan <ali.ihsan.mv@gmail.com>
2023-10-13 10:19:33 +0500

    redirect to menu on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
+import { redirect } from 'next/navigation'
+
 export default function Home() {
+  redirect('/menu');
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div>
